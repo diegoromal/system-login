c3915c95d1518a6a0da05f48b12d6d5636c935f5
Diego RA <diego@qosit.com.br>
2026-01-09 07:59:21 -0300

    Added frontend initial

---
+++ frontend-test/src/app/page.tsx
@@ -0,0 +1,14 @@
+import { LoginCard } from "@/components/login/login-card";
+import { Button } from "@/components/ui/button";
+
+export default function Home() {
+  return (
+    <div className="w-screen h-screen flex items-center justify-center bg-foreground/5">
+      <LoginCard />
+    </div>
+    
+
+
+    
+  );
+}
---
+++ frontend-test/src/components/login/login-card.tsx
@@ -0,0 +1,61 @@
+import { Label } from "@radix-ui/react-label";
+import { Card, CardAction, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "../ui/card";
+import { Input } from "../ui/input";
+import { Button } from "../ui/button";
+import Link from "next/link";
+
+export function LoginCard() {
+    return (
+        <Card className="w-full max-w-sm">
+            <CardHeader>
+                <CardTitle>
+                    Faça o Login
+                </CardTitle>
+                <CardDescription>
+                    Entre com os dados
+                </CardDescription>
+                <CardAction>
+                    <Link href={"/cadastro"}>
+                        <Button variant="link">Criar Conta</Button>
+                    </Link>
+                </CardAction>
+            </CardHeader>
+            <CardContent>
+                <form>
+                    <div className="flex flex-col gap-6">
+                        <div className="grid gap-2">
+                            <div className="flex items-center">
+                                <Label htmlFor="email">
+                                    Email
+                                </Label>
+                            </div>
+                            <Input
+                                id="email"
+                                type="email"
+                                placeholder="m@example.com"
+                                required
+                            />
+                        </div>
+                        <div className="grid gap-2">
+                            <div className="flex items-center">
+                                <Label htmlFor="password">
+                                    Senha
+                                </Label>
+                            </div>
+                            <Input
+                                id="password"
+                                type="password"
+                                required
+                            />
+                        </div>
+                    </div>
+                </form>
+            </CardContent>
+            <CardFooter className="flex-col gap-2">
+                <Button type="submit" className="w-full">
+                    Login
+                </Button>
+            </CardFooter>
+        </Card>
+    )
+}
